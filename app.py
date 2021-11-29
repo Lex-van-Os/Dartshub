3f04c021ead1ffee0586b6a0075b7a709c8f40cd
Faroeq <alves_1044@hotmail.com>
2021-11-29 18:44:43 +0100

    Add auto reload

--- app.py
+++ app.py
@@ -9,10 +9,17 @@ def index():
     return "Ga naar /agenda voor de agenda."
 
 
+@app.route("/test")
+def test():
+    return render_template("test.html.jinja")
+
+
 @app.route("/agenda")
 def agenda():
     return render_template("agenda.html.jinja")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(debug=True)
