d5cbfab814d4036f14adb659d9ecb8bbab611c90
Babette van Aubel <babettevanaubel@gmail.com>
2021-11-30 21:15:37 +0100

    -created the form route -created the basic form webpage

--- app.py
+++ app.py
@@ -3,14 +3,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return "Ga naar /agenda voor de agenda."
+
 
 @app.route("/agenda")
 def agenda():
     return render_template("agenda.html")
 
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
