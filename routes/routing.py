0468e6dbf98a107f96346b21d44f349504cc70d4
Faroeq <alves_1044@hotmail.com>
2021-12-09 18:38:04 +0100

    move form.html to event create template

--- routes/routing.py
+++ routes/routing.py
@@ -1,12 +1,20 @@
 from config import app
 from flask import render_template
 
+
 # Routes of the webpages
 @app.route("/")
 @app.route("/home")
 def index():
 
     return render_template('home.html.jinja')
+
+
+@app.route(
+    "/event/edit"
+)  # niet de officiele route! Dit moet aangepast worden op event/edit/{id}
+def event():
+    return render_template("event-edit.html.jinja")
 
 
 @app.route("/agenda")
