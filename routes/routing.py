1d534278ccb711ab510e3c8749cf518330ce157d
Faroeq <alves_1044@hotmail.com>
2021-12-09 18:43:59 +0100

    add event create route

--- routes/routing.py
+++ routes/routing.py
@@ -6,15 +6,7 @@ from flask import render_template
 @app.route("/")
 @app.route("/home")
 def index():
-
     return render_template('home.html.jinja')
-
-
-@app.route(
-    "/event/edit"
-)  # niet de officiele route! Dit moet aangepast worden op event/edit/{id}
-def event():
-    return render_template("event-edit.html.jinja")
 
 
 @app.route("/agenda")
@@ -22,6 +14,6 @@ def agenda():
     return render_template("agenda.html.jinja")
 
 
-@app.route("/form")
+@app.route("/event/create")
 def form():
-    return render_template("form.html")
+    return render_template("event/event_create.html.jinja")
