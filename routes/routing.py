5f98eb34a93148bf04080333196f41230846a009
Babette van Aubel <babettevanaubel@gmail.com>
2021-12-11 18:33:59 +0100

    added a new route

--- routes/routing.py
+++ routes/routing.py
@@ -17,3 +17,16 @@ def agenda():
 @app.route("/event/create")
 def form():
     return render_template("event/event_create.html.jinja")
+
+@app.route("/create_event", methods=['GET', 'POST'])
+def form():
+    form = EventsForm(FlaskForm)
+    return render_template("create_event.html", title="Create Event", form=form)
+
+    # Gets event name
+
+    # The database we use to gather the information from forms: flaskwtforms
+
+    # The form.html web page needs the backend to get the data from the form submitted
+    # to be displayed to the user by sending them an email
+    # POST & GET methods are required
