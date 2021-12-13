95d25a422d2d99c7c1b4edee5bdbb5b81f18195a
Babette van Aubel <babettevanaubel@gmail.com>
2021-12-13 10:44:07 +0100

    Added routing & edit the create-event-jinja file

--- routes/routing.py
+++ routes/routing.py
@@ -1,12 +1,11 @@
 from config import app
 from flask import render_template
 
-
 # Routes of the webpages
 @app.route("/")
 @app.route("/home")
 def index():
-    return render_template('home.html.jinja')
+    return render_template("home.html.jinja")
 
 
 @app.route("/agenda")
@@ -18,15 +17,16 @@ def agenda():
 def form():
     return render_template("event/event_create.html.jinja")
 
-@app.route("/create_event", methods=['GET', 'POST'])
-def form():
-    form = EventsForm(FlaskForm)
-    return render_template("create_event.html", title="Create Event", form=form)
 
-    # Gets event name
+# @app.route("/create_event", methods=["GET", "POST"])
+# def form():
+#     form = EventsForm(FlaskForm)
+#     return render_template("create_event.html", title="Create Event", form=form)
 
-    # The database we use to gather the information from forms: flaskwtforms
+# Gets event name
 
-    # The form.html web page needs the backend to get the data from the form submitted
-    # to be displayed to the user by sending them an email
-    # POST & GET methods are required
+# The database we use to gather the information from forms: flaskwtforms
+
+# The form.html web page needs the backend to get the data from the form submitted
+# to be displayed to the user by sending them an email
+# POST & GET methods are required
